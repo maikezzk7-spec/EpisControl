b096f7c6f35e258c90fb54a8779dd3a731b0f07e
maikezzk7-spec <maikezzk7@gmail.com>
2026-04-08 21:10:48 -0300

    ajustado

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
 <router-view />
 </template>
+<style>
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #050a18;
+}
+
+#app {
+  margin: 0;
+  padding: 0;
+}
+</style>
